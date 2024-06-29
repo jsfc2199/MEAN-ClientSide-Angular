770ea33c4cc13539f833b85700d0405473d844ae
Juanse Franco <jsfc2199@gmail.com>
2024-06-29 13:56:16 -0500

    logout implementado. Finalizado MEAN de acuerdo al curso de Angular Básico de Fernando herrera. Se puede extrapolar más esta app

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -47,7 +47,10 @@ export class AuthService {
     const url = `${this.baseUrl}/auth/check-token`
     const token = localStorage.getItem('token')
 
-    if(!token) return of(false);
+    if(!token) {
+      this.logout()
+      return of(false)
+    };
 
     //verificamos el token y lo colocamos en los headers
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`)
@@ -60,5 +63,11 @@ export class AuthService {
         return of(false)
       })
     )
+  }
+
+  logout(){
+    this._authStatus.set(AuthStatus.notAuthenticated);
+    this._currentUser.set(null);
+    localStorage.removeItem('token')
   }
 }
--- src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, inject } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './dashboard-layout.component.html',
@@ -8,9 +9,15 @@ import { AuthService } from '../../../auth/services/auth.service';
 export class DashboardLayoutComponent {
 
   private authService: AuthService = inject(AuthService)
+  private router = inject(Router)
   public user = computed(() => this.authService.currentUser())
 
   // get user() {
   //   return this.authService.currentUser()
   // }
+
+  logout(){
+    this.authService.logout()
+    // this.router.navigateByUrl('/auth/login')
+  }
 }
