0c87634e2f9e8a0390bff741d0e20d1a7699eb5d
Juanse Franco <jsfc2199@gmail.com>
2024-06-29 13:01:49 -0500

    Servicio para verificar token de acceso

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, computed, inject, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
-import { AuthStatus, LoginResponse, User } from '../interfaces';
+import { AuthStatus, CheckTokenResponse, LoginResponse, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -34,6 +34,31 @@ export class AuthService {
       }),
       map(() => true),
       catchError(err => throwError(() => err.error.message))
+    )
+  }
+
+  checkAuthStatus(): Observable<boolean> {
+    const url = `${this.baseUrl}/auth/check-token`
+    const token = localStorage.getItem('token')
+
+    if(!token) return of(false);
+
+    //verificamos el token y lo colocamos en los headers
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`)
+
+    return this.http.get<CheckTokenResponse>(url, {headers})
+    .pipe(
+      map(({user, token}) =>{
+        //establecemos la información si el usuario tiene un token para seguir autenticado
+        this._currentUser.set(user);
+        this._authStatus.set(AuthStatus.authenticated);
+        localStorage.setItem('token', token);
+        return true;
+      }),
+      catchError(() => {
+        this._authStatus.set(AuthStatus.notAuthenticated);
+        return of(false)
+      })
     )
   }
 }
